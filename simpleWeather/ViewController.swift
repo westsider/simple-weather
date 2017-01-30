8800c06cac09ca4072b4499f9134e691034d8883
Warren Hansen <15slim@Warrens-MacBook-Pro.local>
2017-01-30 13:02:22 -0800

    Initial Commit

---
+++ simpleWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleWeather
+//
+//  Created by Warren Hansen on 1/30/17.
+//  Copyright © 2017 Warren Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
